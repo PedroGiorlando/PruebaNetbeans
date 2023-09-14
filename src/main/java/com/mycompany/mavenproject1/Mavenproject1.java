8cb7a4d6d523a65858a511ed51007d624370062a
PedroGiorlando <pggv2003@gmail.com>
2023-09-13 21:37:24 -0300

    Prueba

---
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.mavenproject1;
+import java.util.Scanner;
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello World!");
+       
+       
+        
+         System.out.print("Ingresa tu nombre: ");
+         String nombre = sc.nextLine();
+    }
+}
